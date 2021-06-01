f118299cef45d5ac9aa4c54de9f58263e3ddd6b1
Miguel Guthridge <hdsq@outlook.com.au>
2021-06-01 23:56:28 +1000

    Add default project option to setup

--- projectsetup.py
+++ projectsetup.py
@@ -26,11 +26,22 @@ def getSetupJson(course, project):
         print(f"Error: setup configuration for {course}/{project} not found")
         exit(0)
 
+def getDefaultJson():
+    f_dir = os.path.dirname(__file__) + "/setups/" + "default.json"
+    if os.path.exists(f_dir):
+        print("Creating localtest.json...")
+        shutil.copyfile(f_dir, "localtest.json")
+    else:
+        print("Error: default.json missing!")
+
 def main(args):
+    if args[0] == "default":
+        getDefaultJson()
+        exit()
     parser = argparse.ArgumentParser("localtest setup", description="Set up a directory to work with localtest")
     parser.add_argument("course", type=str)
     parser.add_argument("project", type=str)
     args = parser.parse_args(args)
-
+    
     getSetupJson(args.course, args.project)
     getStarterCode()
