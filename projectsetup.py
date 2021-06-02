faa78c9ae9fde7eb569555ffc030b1f2a5134bb7
Miguel Guthridge <hdsq@outlook.com.au>
2021-06-02 15:02:43 +1000

    Add check for non-empty directory

--- projectsetup.py
+++ projectsetup.py
@@ -34,14 +34,27 @@ def getDefaultJson():
     else:
         print("Error: default.json missing!")
 
+def checkDirContents():
+    try:
+        # Check if directory has files or folders
+        os.scandir(".").__next__()
+        print("Warning: the directory already has files or folders present.")
+        print("Continuing may overwrite existing files.")
+        if input("Do you wish to continue? (y/n) ").lower() not in ["y", "yes"]:
+            exit()
+    except IndexError:
+        # Directory is empty
+        pass
+
 def main(args):
-    if args[0] == "default":
+    if len(args) > 0 and args[0] == "default":
+        checkDirContents()
         getDefaultJson()
         exit()
     parser = argparse.ArgumentParser("localtest setup", description="Set up a directory to work with localtest")
     parser.add_argument("course", type=str)
     parser.add_argument("project", type=str)
     args = parser.parse_args(args)
-    
+    checkDirContents()
     getSetupJson(args.course, args.project)
     getStarterCode()
